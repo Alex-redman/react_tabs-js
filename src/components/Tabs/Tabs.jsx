7024187fdadb5f9b40ed53327f7122b5f4f5bb77
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-12-29 16:38:54 +0200

    react tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,33 @@
-export const Tabs = () => {};
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+
+export const Tabs = ({ tabs, onTabSelect }) => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  const handleTabClick = tabId => {
+    setActiveTab(tabId);
+    onTabSelect(tabId);
+  };
+
+  return (
+    <div className="tabs is-boxed">
+      <ul>
+        {tabs.map(tab => (
+          <li
+            className={classNames({ 'is-active': activeTab === tab.id })}
+            data-cy="Tab"
+            key={tab.id}
+          >
+            <a
+              onClick={() => handleTabClick(tab.id)}
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
