44665f1c0120de364f3906d89cba50004587529f
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-12-29 16:51:54 +0200

    react tabs js

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,25 @@ export const tabs = [
 
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
-  const activeTab = tabs.find(tab => tab.id === activeTabId);
+
+  const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
+
+  const handleTabSelected = tabId => {
+    if (tabId !== activeTabId) {
+      setActiveTabId(tabId);
+    }
+  };
 
   return (
     <div className="section">
       <h1 className="title">Selected tab is {activeTab.title}</h1>
 
       <div data-cy="TabsComponent">
-        <Tabs tabs={tabs} onTabSelect={setActiveTabId} />
+        <Tabs
+          tabs={tabs}
+          activeTabId={activeTabId}
+          onTabSelected={handleTabSelected}
+        />
 
         <div className="block" data-cy="TabContent">
           {activeTab.content}
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,25 +1,17 @@
 import classNames from 'classnames/bind';
-import { useState } from 'react';
 
-export const Tabs = ({ tabs, onTabSelect }) => {
-  const [activeTab, setActiveTab] = useState(tabs[0].id);
-
-  const handleTabClick = tabId => {
-    setActiveTab(tabId);
-    onTabSelect(tabId);
-  };
-
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   return (
     <div className="tabs is-boxed">
       <ul>
         {tabs.map(tab => (
           <li
-            className={classNames({ 'is-active': activeTab === tab.id })}
-            data-cy="Tab"
             key={tab.id}
+            className={classNames({ 'is-active': activeTabId === tab.id })}
+            data-cy="Tab"
           >
             <a
-              onClick={() => handleTabClick(tab.id)}
+              onClick={() => onTabSelected(tab.id)}
               href={`#${tab.id}`}
               data-cy="TabLink"
             >
